613a0e8ae934fe39cd78c41f85de273a77c80576
FX Foo <fangxuan.foo@gmail.com>
2022-02-08 20:59:53 +0800

    updated fetch from S3

--- src/components/dataprocessing/getdata.jsx
+++ src/components/dataprocessing/getdata.jsx
@@ -7,47 +7,65 @@ function GetData(props) {
   const [loadingState, setLoadingState] = useState(false);
   const [loadedData, setLoadedData] = useState([]);
 
-  const AWS = require("aws-sdk");
-  AWS.config.update({ region: "ap-southeast-2" });
-  const s3 = new AWS.S3({
-    accessKeyId: "AKIA3DUOXD2AS6GTSJWK",
-    secretAccessKey: "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",
-    // accessKeyId: process.env.REACT_APP_S3KEYID,
-    // secretAccessKey: process.env.REACT_APP_S3AKEY,
+//   const AWS = require("aws-sdk");
+//   AWS.config.update({ region: "ap-southeast-2" });
+//   const s3 = new AWS.S3({
+//     // accessKeyId: process.env.REACT_APP_S3KEYID,
+//     // secretAccessKey: process.env.REACT_APP_S3AKEY,
 
-    Bucket: "reaction-on-elearning",
-  });
+//     Bucket: "reaction-on-elearning",
+//   });
 
-  useEffect(() => {
-    setLoadingState(true);
-    const fetchData = async () => {
-      try {
-        const params = {
-          Bucket: "reaction-on-elearning",
-          Key: "react-on-elearning.json",
-        };
+//   useEffect(() => {
+//     setLoadingState(true);
+//     const fetchData = async () => {
+//       try {
+//         const params = {
+//           Bucket: "reaction-on-elearning",
+//           Key: "react-on-elearning.json",
+//         };
 
-        const data = await s3.getObject(params).promise();
-        const response = JSON.parse(data.Body.toString("utf-8"));
-        return response;
-      } catch (e) {
-        throw new Error(`Could not retrieve file from S3: ${e.message}`);
-      }
-    };
-    fetchData().then((values) => {
-      const displaydata = [];
-      for (const key in values) {
-        const convertedData = {
-          id: key,
-          ...values[key],
-        };
-        displaydata.push(convertedData);
-      }
-      setLoadedData(displaydata);
-      setLoadingState(false);
-    });
-  }, []);
+//         const data = await s3.getObject(params).promise();
+//         const response = JSON.parse(data.Body.toString("utf-8"));
+//         return response;
+//       } catch (e) {
+//         throw new Error(`Could not retrieve file from S3: ${e.message}`);
+//       }
+//     };
+//     fetchData().then((values) => {
+//       const displaydata = [];
+//       for (const key in values) {
+//         const convertedData = {
+//           id: key,
+//           ...values[key],
+//         };
+//         displaydata.push(convertedData);
+//       }
+//       setLoadedData(displaydata);
+//       setLoadingState(false);
+//     });
+//   }, []);
+useEffect(() => {
+        setLoadingState(true);
+        fetch ("https://reaction-on-elearning.s3.ap-southeast-2.amazonaws.com/react-on-elearning.json")
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            const displaydata = [];
 
+            for (const key in data) {
+                const convertedData = {
+                    id: key,
+                    ...data[key],
+                };
+                displaydata.push(convertedData);
+            }
+            setLoadingState(false);
+            setLoadedData(displaydata);
+        });
+
+    }, []);
   if (loadingState) {
     return <div>Currently Loading Data... </div>;
   }
